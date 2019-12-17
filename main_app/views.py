39d75de2f5f38386e54188369c5e2858a2d6b781
Regina Campos <rmscampos@gmail.com>
2019-12-17 14:54:31 -0600

    adds home and about routes

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+  return render(request, 'about.html')
