3825159ebe04031dae63f124b344485c240b69e3
Regina Campos <rmscampos@gmail.com>
2019-12-18 19:09:26 -0600

    adds fake data

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('pyrexes/', views.pyrexes_index, name='index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
 
+class Pyrex:
+    def __init__(self, pattern, shape, year, origin): 
+        self.pattern = pattern
+        self.shape = shape
+        self.year = year
+        self.origin = origin
 
+pyrexes = [
+    Pyrex('Gooseberry', 'Oval Casserole', 1957, 'US'),
+    Pyrex('Butterprint', 'Cinderella Round Casserole', 1957, 'US'),
+    Pyrex('Friendship', 'Round Nesting Mixing Bowl', 1971, 'US')
+]
+ 
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def pyrexes_index(request):
+  return render(request, 'pyrexes/index.html', {
+      'pyrexes': pyrexes
+  })
