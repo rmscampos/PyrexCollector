bf98852957b441cd365f6c35264231cebe4effe6
Regina Campos <rmscampos@gmail.com>
2019-12-19 13:04:37 -0600

    adds details view

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('pyrexes/', views.pyrexes_index, name='index')
+    path('pyrexes/', views.pyrexes_index, name='index'),
+    path('pyrexes/<int:pyrex_id>/', views.pyrexes_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,5 @@
 from django.shortcuts import render
-
-class Pyrex:
-    def __init__(self, pattern, shape, year, origin): 
-        self.pattern = pattern
-        self.shape = shape
-        self.year = year
-        self.origin = origin
-
-pyrexes = [
-    Pyrex('Gooseberry', 'Oval Casserole', 1957, 'US'),
-    Pyrex('Butterprint', 'Cinderella Round Casserole', 1957, 'US'),
-    Pyrex('Friendship', 'Round Nesting Mixing Bowl', 1971, 'US')
-]
+from .models import Pyrex
  
 # Define the home view
 def home(request):
@@ -21,6 +9,10 @@ def about(request):
   return render(request, 'about.html')
 
 def pyrexes_index(request):
-  return render(request, 'pyrexes/index.html', {
-      'pyrexes': pyrexes
-  })
+  pyrexes = Pyrex.objects.all()
+  return render(request, 'pyrexes/index.html', {'pyrexes': pyrexes})
+
+def pyrexes_detail(request, pyrex_id):
+  pyrex = Pyrex.objects.get(id=pyrex_id)
+  return render(request, 'pyrexes/detail.html', { 'pyrex': pyrex })
+
